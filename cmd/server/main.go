505144c753b2c1a7ad931fec76e5861b600d46e4
liufei <liufei@sensetime.com>
2021-11-17 18:45:52 +0800

    基本实现socket的点对点聊天

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,8 +7,9 @@ import (
 	"net"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 	"syscall"
-	"time"
 )
 
 type Server struct {
@@ -55,9 +56,8 @@ func (s *Server) DoReceive() {
 			scan.Split(bufio.ScanLines)
 			for scan.Scan() {
 				str := scan.Text()
-				str = fmt.Sprint(c.RemoteAddr(), c.LocalAddr()) + " " + str
 				s.msg <- str
-				log.Println(str)
+				log.Println("get", fmt.Sprint(c.RemoteAddr(), c.LocalAddr())+" "+str)
 			}
 			defer c.Close()
 		}(c)
@@ -66,40 +66,24 @@ func (s *Server) DoReceive() {
 
 func (s *Server) DoReplay() {
 	for str := range s.msg {
-		for i := 0; i < len(s.cons); i++ {
-			s.cons[i].SetWriteDeadline(time.Now().Add(2000))
-			s.cons[i].Write([]byte(str + string('\n')))
-		}
-	}
-}
-
-func createServer() {
-	listen, err := net.Listen("tcp", ":20200")
-	if err != nil {
-		log.Error(err)
-		return
-	}
-	go func() {
-		for {
-			c, err := listen.Accept()
+		if strings.HasPrefix(str, "To") {
+			id := str[3:7]
+			index, err := strconv.Atoi(id)
 			if err != nil {
 				log.Error(err)
 			}
-			log.Println(c.RemoteAddr(), c.LocalAddr())
-			go func(c net.Conn) {
-				scan := bufio.NewScanner(c)
-				scan.Split(bufio.ScanLines)
-				for scan.Scan() {
-					str := scan.Text()
-					log.Println(str)
-				}
-				c.Close()
-			}(c)
+			//s.cons[index].SetWriteDeadline(time.Now().Add(2000))
+			n, err := s.cons[index].Write([]byte(str + string('\n')))
+			if err != nil {
+				log.Error(err)
+			}
+			log.Println("send to ", index, " "+str, "len is ", n)
+			continue
 		}
-	}()
-	signals := []os.Signal{syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT}
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, signals...)
-	s := <-ch
-	log.Fatal(s)
+
+		for i := 0; i < len(s.cons); i++ {
+			//s.cons[i].SetWriteDeadline(time.Now().Add(2000))
+			s.cons[i].Write([]byte(str + string('\n')))
+		}
+	}
 }
